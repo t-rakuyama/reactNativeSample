c1b0f5b094022d4197753c11a27ec824d681e034
t-rakuyama <t.rakuyama929@gmail.com>
2020-09-24 18:39:41 +0900

    ページ遷移のリファクタ

--- src/screens/Stack2.js
+++ src/screens/Stack2.js
@@ -1,31 +1,57 @@
 import React from 'react';
-
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TextInput, Button} from 'react-native';
 
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
 class Stack2 extends React.Component {
+  state = {
+    inputValue: '',
+    texts: [],
+  };
+
+  // 入力文字をリストに保管する関数
+  _onPress = value => {
+    const texts = this.state.texts;
+    texts.unshift( this.state.inputValue );
+    this.setState({ texts })
+  };
+
+  // 文字入力時に入力文字を保管する関数
+  _onChangeText = inputValue => {
+    this.setState({ inputValue })
+  };
+
   render() {
     return (
-      <View style={styles.flexCenter}>
-        <Text style={styles.sectionTitle}>Slack2</Text>
-      </View>
+      <>
+        {/* 文字入力フィールド */}
+        <TextInput
+          style={styles.textInput}
+          value={this.state.inputValue}
+          onChangeText={this._onChangeText}
+        />
+        {/* 追加ボタン */}
+        <Button
+          onPress={this._onPress}
+          title="追加"
+        />
+        {/* 表示領域 TODO ここをコンポーネントに切り出すと綺麗になるかも */}
+        <View>
+          {this.state.texts.map( (value, index) => {return (<Text key={index}>{value}</Text>) })}
+        </View>
+      </>
     );
   }
 }
 
+// スタイル
 const styles = StyleSheet.create( {
-  flexCenter: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
 
